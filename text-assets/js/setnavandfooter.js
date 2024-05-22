0334fe052bd0222d9ce4f9a16e73f9b2b8c7196f
wraithcode2050 <148579273+wraithcode20hehe@users.noreply.github.com>
2024-05-21 21:30:48 -0400

    Update setnavandfooter.js

--- text-assets/js/setnavandfooter.js
+++ text-assets/js/setnavandfooter.js
@@ -1,29 +1,48 @@
 function updateNavAndFooter() {
-  document.getElementById("navbar").innerHTML = `
-    <div class="left">
-      <img src="/logo_large.png">
-      <a href="/">Home</a>
-      <a href="/about">About</a>
-    </div>
-    <div class="right">
-      <a href="/nexturl">Next URL</a>
-      <a href="/chat">Chat</a>
-      <a href="/eaglauncher">Eaglercraft</a>
-      <a href="/more">More</a>
-    </div>
-  `;
-  
-  document.getElementById("footer").innerHTML = `
-    <h4 style="margin-bottom: 5px;">Contact</h4>
-      <a><i class="fa-brands fa-github"></i> &nbsp;&nbsp; <a href="https://github.com/wraithcode20haha">Github</a></a><br>
-      <a><i class="fa-brands fa-stack-overflow"></i> &nbsp;&nbsp; <a href="https://stackoverflow.com/users/22487123/wraithcode">Stack Overflow</a></a><br>
-      <a><i class="fa-brands fa-youtube"></i> &nbsp;&nbsp; <a href="https://www.youtube.com/channel/UC1M1KgAJWRcQ_GUwea8_irw">Youtube</a></a><br>
-      <a><i class="fa fa-wheat-awn"></i> &nbsp;&nbsp; <a href="https://wraithcode.straw.page">Straw Page</a></a>
-      <h2>&lt;More footer content here&gt;</h2>
-      <p id="ip-display">fetching ip...</p>
-      <script src="/text-assets/js/getip.js"></script>
-      <script>
-        const ipListElement = document.getElementById('ip-display'); function displayIPs() { const public1 = { value: null }; const public2 = { value: null }; const private1 = { value: null }; getIP('private1', private1); ipListElement.innerHTML = "Your IP: " + (private1.value || 'Could not fetch IP');} setTimeout(displayIPs, 1000);
-      </script>
-  `;
-}
+            document.getElementById("navbar").innerHTML = `
+                <div class="left">
+                    <img src="/logo_large.png">
+                    <a href="/">Home</a>
+                    <a href="/about">About</a>
+                </div>
+                <div class="right">
+                    <a href="/nexturl">Next URL</a>
+                    <a href="/chat">Chat</a>
+                    <a href="/eaglauncher">Eaglercraft</a>
+                    <a href="/more">More</a>
+                </div>
+            `;
+            
+            const footer = document.getElementById("footer");
+            footer.innerHTML = `
+                <h4 style="margin-bottom: 5px;">Contact</h4>
+                <a><i class="fa-brands fa-github"></i> &nbsp;&nbsp; <a href="https://github.com/wraithcode20haha">Github</a></a><br>
+                <a><i class="fa-brands fa-stack-overflow"></i> &nbsp;&nbsp; <a href="https://stackoverflow.com/users/22487123/wraithcode">Stack Overflow</a></a><br>
+                <a><i class="fa-brands fa-youtube"></i> &nbsp;&nbsp; <a href="https://www.youtube.com/channel/UC1M1KgAJWRcQ_GUwea8_irw">Youtube</a></a><br>
+                <a><i class="fa fa-wheat-awn"></i> &nbsp;&nbsp; <a href="https://wraithcode.straw.page">Straw Page</a></a>
+                <h2>&lt;More footer content here&gt;</h2>
+                <p id="ip-display">fetching ip...</p>
+            `;
+
+            // Dynamically load external script
+            const script1 = document.createElement("script");
+            script1.src = "/text-assets/js/getip.js";
+
+            // Inline script for displaying IP after the external script is loaded
+            const script2 = document.createElement("script");
+            script2.textContent = `
+                const ipListElement = document.getElementById('ip-display');
+                function displayIPs() {
+                    const public1 = { value: null };
+                    const public2 = { value: null };
+                    const private1 = { value: null };
+                    getIP('private1', private1);
+                    ipListElement.innerHTML = "Your IP: " + (private1.value || 'Could not fetch IP');
+                }
+                setTimeout(displayIPs, 1000);
+            `;
+
+            // Append the scripts to the footer
+            footer.appendChild(script1);
+            footer.appendChild(script2);
+        }
