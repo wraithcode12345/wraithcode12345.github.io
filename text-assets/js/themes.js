051b2317bb1f88b8ab6f05738ef0f071d016e25b
wraithcode2050 <148579273+wraithcode20hehe@users.noreply.github.com>
2024-05-22 01:12:40 -0400

    Create themes.js

---
+++ text-assets/js/themes.js
@@ -0,0 +1,4 @@
+function applyTheme() {
+  // idk do this later T_T
+}
+// future me, remember to add this function to the domcontentloaded event listener that has the function to apply nav and footer
