3c1a851322a145f1d669d13b52f2d0f2d26f50e0
wraithcode2050 <148579273+wraithcode20hehe@users.noreply.github.com>
2024-05-22 03:06:10 -0400

    Update and rename themes.js to theme.js

--- text-assets/js/themes.js
+++
@@ -1,4 +0,0 @@
-function applyTheme() {
-  // idk do this later T_T
-}
-// future me, remember to add this function to the domcontentloaded event listener that has the function to apply nav and footer
