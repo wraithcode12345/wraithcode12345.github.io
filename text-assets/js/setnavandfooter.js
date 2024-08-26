ce0b0ec15eed4c936065b951526199b57c4024f8
wraithcode2050 <148579273+wraithcode2042@users.noreply.github.com>
2024-08-26 15:14:41 -0400

    Update setnavandfooter.js

--- text-assets/js/setnavandfooter.js
+++ text-assets/js/setnavandfooter.js
@@ -16,7 +16,6 @@ function updateNavAndFooter() {
             
             const footer = document.getElementById("footer");
             footer.innerHTML = `
-                <div class='summeralert'><p>Summer break is here! I will be pretty busy from June to September, so expect development be much slower than usual.</p></div> 
                 <h4 style="margin-bottom: 5px;">Contact</h4>
                 <a><i class="fa-brands fa-github"></i> &nbsp;&nbsp; <a href="https://github.com/wraithcode20haha">Github</a></a><br>
                 <a><i class="fa-brands fa-stack-overflow"></i> &nbsp;&nbsp; <a href="https://stackoverflow.com/users/22487123/wraithcode">Stack Overflow</a></a><br>
