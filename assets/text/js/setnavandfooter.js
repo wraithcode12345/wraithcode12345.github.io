cdc746a796ad6a00b2604077e9032678605df0f1
wraithcode2050 <148579273+wraithcode2018@users.noreply.github.com>
2024-09-09 22:25:16 -0400

    Update setnavandfooter.js

--- assets/text/js/setnavandfooter.js
+++ assets/text/js/setnavandfooter.js
@@ -4,20 +4,22 @@ function updateNavAndFooter() {
     navm.innerHTML = `
         <button class="navbtn" onclick="window.location.href='/';"><i class="wicon wicon-home"></i><br><span class="navtext">Home</span></button>
         <div class="right">
-            <button class="navbtn" onclick="window.location.href='/axis';"><i class="wicon wicon-axis"></i><br><span class="navtext">Axis</span></button>
-            <button class="navbtn" onclick="window.location.href='/games';"><i class="wicon wicon-gamepad-handles"></i><br><span class="navtext">Games</span></button>
-            <button class="navbtn" onclick="window.location.href='/tools';"><i class="wicon wicon-tools"></i><br><span class="navtext">Tools</span></button>
-            <button class="navbtn" onclick="window.location.href='/settings';"><i class="wicon wicon-gear"></i><br><span class="navtext">Settings</span></button>
+            <button class="navbtn" onclick="window.location.href='/chat/';"><i class="wicon wicon-chat"></i><br><span class="navtext">Chat</span></button>
+            <button class="navbtn" onclick="window.location.href='/axis/';"><i class="wicon wicon-axis"></i><br><span class="navtext">Axis</span></button>
+            <button class="navbtn" onclick="window.location.href='/games/';"><i class="wicon wicon-gamepad-handles"></i><br><span class="navtext">Games</span></button>
+            <button class="navbtn" onclick="window.location.href='/tools/';"><i class="wicon wicon-tools"></i><br><span class="navtext">Tools</span></button>
+            <button class="navbtn" onclick="window.location.href='/settings/';"><i class="wicon wicon-gear"></i><br><span class="navtext">Settings</span></button>
         </div>
     `;
     const navs = document.getElementById("sidenav");
     navs.innerHTML = `
         <div style="height: 55px; width: 100%;"></div> <!-- spacer -->
         <button onclick="window.location.href='/';"><i class="wicon wicon-home"></i><span>Home</span></button>
-        <button onclick="window.location.href='/axis';"><i class="wicon wicon-axis"></i><span>Axis</span></button>
-        <button onclick="window.location.href='/games';"><i class="wicon wicon-gamepad-handles"></i><span>Games</span></button>
-        <button onclick="window.location.href='/tools';"><i class="wicon wicon-tools"></i><span>Tools</span></button>
-        <button onclick="window.location.href='/settings';"><i class="wicon wicon-gear"></i><span>Settings</span></button>
+        <button onclick="window.location.href='/chat/';"><i class="wicon wicon-chat"></i><span>Chat</span></button>
+        <button onclick="window.location.href='/axis/';"><i class="wicon wicon-axis"></i><span>Axis</span></button>
+        <button onclick="window.location.href='/games/';"><i class="wicon wicon-gamepad-handles"></i><span>Games</span></button>
+        <button onclick="window.location.href='/tools/';"><i class="wicon wicon-tools"></i><span>Tools</span></button>
+        <button onclick="window.location.href='/settings/';"><i class="wicon wicon-gear"></i><span>Settings</span></button>
     `;
             
     const footer = document.getElementById("footer");
